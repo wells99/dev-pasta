ad1af59dc9f17d6f2c8b0c0fbd0f48e7dce3e709
unknown <wellingtonsilva0678@digitalcollege.internal>
2025-01-30 09:49:25 -0300

    working

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [
+      'dev-pasta.onrender.com', 
+    ],
+  },
 })
-
