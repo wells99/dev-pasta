b0b034e64cc4fd22f557e0b91821ec3546473e41
unknown <wellingtonsilva0678@digitalcollege.internal>
2025-01-30 09:45:31 -0300

    teste de upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { secret } from "./secrets.jsx";
 import GoogleDriveFolder from './GoogleDriveFolder.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleDriveFolder folderId="" apiKey="AIzaSyBDz6knTZHHr I0ruvWE9WtTZyVC6znOu1Y" />
+    <GoogleDriveFolder folderId={secret.folderId} apiKey={secret.apiKey} />
   </StrictMode>,
 )
